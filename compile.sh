b5f373f6e5881ab87f49c418db78695f2475f20f
Nicula Ionut <niculaionut@tutanota.com>
2023-05-02 02:00:53 +0300

    Add comment, enable optimizations

--- compile.sh
+++ compile.sh
@@ -2,10 +2,15 @@
 
 set -xe
 
-CXXFLAGS="-std=c++20 -O0 -ggdb -march=native -Wall -Wextra -Wconversion"
+CXXFLAGS="-std=c++20 -Os -fno-exceptions -fno-rtti -flto -march=native -Wall -Wextra -Wconversion"
 CXX=clang++
 
 bison -d pai.y
 flex pai.l
+
+# The C++ lexer generator is a bit broken. It defines the function `yywrap()`
+# twice (once in pai_lexer.h and once in pai_lexer.cpp), leading to a linker
+# error. The following command gets around this issue.
 sed 's/.*yywrap.*return.*//g' pai_lexer.cpp -i
+
 $CXX $CXXFLAGS "$@" pai.cpp pai_lexer.cpp pai_parser.cpp -o pai -lfmt
