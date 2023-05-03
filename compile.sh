2b314c37b3bf41d740c8bea0f184d14df8472b26
Nicula Ionut <niculaionut@tutanota.com>
2023-05-03 12:58:48 +0300

    Add link to issue

--- compile.sh
+++ compile.sh
@@ -8,9 +8,10 @@ CXX=clang++
 bison -d pai.y
 flex pai.l
 
-# The C++ lexer generator is a bit broken. It defines the function `yywrap()`
-# twice (once in pai_lexer.h and once in pai_lexer.cpp), leading to a linker
-# error. The following command gets around this issue.
+# The C++ lexer generator is a bit broken
+# (https://github.com/westes/flex/issues/472). It defines the function
+# `yywrap()` twice (once in pai_lexer.h and once in pai_lexer.cpp), leading to
+# a linker error. The following command gets around this issue.
 sed 's/.*yywrap.*return.*//g' pai_lexer.cpp -i
 
 $CXX $CXXFLAGS "$@" pai.cpp pai_lexer.cpp pai_parser.cpp -o pai -lfmt
